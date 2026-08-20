e9a0901a0da27379bb36b0f590491daf75e295a2
Karabo Molefi <karabomolefi@icloud.com>
2026-08-20 19:56:55 +0200

    add service class

---
+++ HKControlSytem/Contestant/Contestant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKControlSytem.Contestant
+{
+    internal class Contestant
+    {
+        protected int ContestantNumber;
+
+        public string Name;
+
+        public int Age;
+
+        public int  YearsofCookingExperience;
+
+        public string Team;
+
+        // Constructor
+
+        Contestant (int contestantNumber, string name, int age, int yearsExperience, string team)
+        {
+            this.ContestantNumber = contestantNumber;
+            this.Name = name;
+            this.Age = age;
+            this.YearsofCookingExperience = yearsExperience;
+            this.Team = team;
+        }
+
+
+    }
+}
